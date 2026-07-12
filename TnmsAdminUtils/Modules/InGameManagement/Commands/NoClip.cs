dd1298df4bf438a4a8bbfc8a4b462c68371005ef
faketuna <faketuna.st@gmail.com>
2026-07-12 16:03:42 +0900

    feat: port movement & spawn commands (noclip, bury, respawn, revive)
    - noclip: toggle/explicit SetMoveType(NoClip/Walk)
    - bury/unbury: teleport Z offset ±10
    - respawn: respawn dead players
    - revive: respawn any player (alive or dead)

---
+++ TnmsAdminUtils/Modules/InGameManagement/Commands/NoClip.cs
@@ -0,0 +1,79 @@
+using Sharp.Shared.Enums;
+using Sharp.Shared.Objects;
+using Sharp.Shared.Types;
+using TnmsAdminUtils.Utils;
+using TnmsPluginFoundation.Extensions.Client;
+using TnmsPluginFoundation.Models.Command;
+using TnmsPluginFoundation.Models.Command.Validators;
+using TnmsPluginFoundation.Utils.Entity;
+
+namespace TnmsAdminUtils.Modules.InGameManagement.Commands;
+
+public class NoClip(IServiceProvider provider) : TnmsAbstractCommandBase(provider)
+{
+    public override string CommandName => "noclip";
+    public override string CommandDescription => "Toggles noclip on a player.";
+
+    public override TnmsCommandRegistrationType CommandRegistrationType =>
+        TnmsCommandRegistrationType.Client | TnmsCommandRegistrationType.Server;
+
+    protected override ICommandValidator? GetValidator() => new CompositeValidator()
+        .Add(new PermissionValidator("tnms.adminutil.management.ingame.command.noclip", true))
+        .Add(new ArgumentCountValidator(1, true))
+        .Add(new TargetValidator(1, true));
+
+    protected override ValidationFailureResult OnValidationFailed(ValidationFailureContext context)
+    {
+        switch (context.Validator)
+        {
+            case ArgumentCountValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(context.Client, "NoClip.Notification.Usage"));
+                break;
+            case PermissionValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(context.Client, "Common.ValidationFailure.NotEnoughPermissions"));
+                break;
+            case TargetValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(context.Client, "Common.ValidationFailure.NoValidTargetsFound"));
+                break;
+        }
+
+        return ValidationFailureResult.SilentAbort();
+    }
+
+    protected override void ExecuteCommand(IGameClient? client, StringCommand commandInfo, ValidatedArguments? validatedArguments)
+    {
+        var targets = validatedArguments!.GetArgument<List<IGameClient>>(1)!;
+
+        int? explicitValue = null;
+        if (commandInfo.ArgCount > 2 && int.TryParse(commandInfo.GetArg(2), out int val))
+            explicitValue = Math.Clamp(val, 0, 1);
+
+        foreach (var gameClient in targets)
+        {
+            var pawn = gameClient.GetPlayerPawn();
+            if (pawn == null)
+                continue;
+
+            bool enableNoclip = explicitValue.HasValue
+                ? explicitValue.Value == 1
+                : pawn.MoveType != MoveType.NoClip;
+
+            pawn.SetMoveType(enableNoclip ? MoveType.NoClip : MoveType.Walk);
+        }
+
+        string executor = PlayerUtil.GetPlayerName(client);
+        string targetName = targets.GetTargetName();
+        string state = explicitValue.HasValue ? (explicitValue.Value == 1 ? "ON" : "OFF") : "toggled";
+        Plugin.TnmsLogger.LogAdminAction(client, $"Admin {executor} set noclip {state} on {targetName}");
+
+        foreach (var gameClient in SharedSystem.GetModSharp().GetIServer().GetGameClients())
+        {
+            if (gameClient.IsFakeClient || gameClient.IsHltv)
+                continue;
+
+            gameClient.GetPlayerController()?
+                .PrintToChat(
+                    ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(gameClient, "NoClip.Broadcast.NoClipSet", executor, targetName, state));
+        }
+    }
+}
---
+++ TnmsAdminUtils/Modules/InGameManagement/Commands/Respawn.cs
@@ -0,0 +1,66 @@
+using Sharp.Shared.Objects;
+using Sharp.Shared.Types;
+using TnmsAdminUtils.Utils;
+using TnmsPluginFoundation.Extensions.Client;
+using TnmsPluginFoundation.Models.Command;
+using TnmsPluginFoundation.Models.Command.Validators;
+using TnmsPluginFoundation.Utils.Entity;
+
+namespace TnmsAdminUtils.Modules.InGameManagement.Commands;
+
+public class Respawn(IServiceProvider provider) : TnmsAbstractCommandBase(provider)
+{
+    public override string CommandName => "respawn";
+    public override string CommandDescription => "Respawns a dead player.";
+
+    public override TnmsCommandRegistrationType CommandRegistrationType =>
+        TnmsCommandRegistrationType.Client | TnmsCommandRegistrationType.Server;
+
+    protected override ICommandValidator? GetValidator() => new CompositeValidator()
+        .Add(new PermissionValidator("tnms.adminutil.management.ingame.command.respawn", true))
+        .Add(new ArgumentCountValidator(1, true))
+        .Add(new TargetValidator(1, true));
+
+    protected override ValidationFailureResult OnValidationFailed(ValidationFailureContext context)
+    {
+        switch (context.Validator)
+        {
+            case ArgumentCountValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(context.Client, "Respawn.Notification.Usage"));
+                break;
+            case PermissionValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(context.Client, "Common.ValidationFailure.NotEnoughPermissions"));
+                break;
+            case TargetValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(context.Client, "Common.ValidationFailure.NoValidTargetsFound"));
+                break;
+        }
+
+        return ValidationFailureResult.SilentAbort();
+    }
+
+    protected override void ExecuteCommand(IGameClient? client, StringCommand commandInfo, ValidatedArguments? validatedArguments)
+    {
+        var targets = validatedArguments!.GetArgument<List<IGameClient>>(1)!;
+
+        foreach (var gameClient in targets)
+        {
+            var controller = gameClient.GetPlayerController();
+            controller?.Respawn();
+        }
+
+        string executor = PlayerUtil.GetPlayerName(client);
+        string targetName = targets.GetTargetName();
+        Plugin.TnmsLogger.LogAdminAction(client, $"Admin {executor} respawned {targetName}");
+
+        foreach (var gameClient in SharedSystem.GetModSharp().GetIServer().GetGameClients())
+        {
+            if (gameClient.IsFakeClient || gameClient.IsHltv)
+                continue;
+
+            gameClient.GetPlayerController()?
+                .PrintToChat(
+                    ((TnmsAdminUtils)Plugin).LocalizeWithPluginPrefix(gameClient, "Respawn.Broadcast.Respawned", executor, targetName));
+        }
+    }
+}
